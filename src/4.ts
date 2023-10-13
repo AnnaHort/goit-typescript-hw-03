3d09abdfea70ed722b7e1476ba117780682147ff
AnnaHort <annahort3d@gmail.com>
2023-10-14 00:15:45 +0300

    add task #4

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,69 @@
+
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+
+abstract class House {
+  door: boolean;
+  key: Key;
+  tenants: Person[] = [];
+
+  constructor(door: boolean, key: Key) {
+    this.door = door;
+    this.key = key;
+  }
+
+  comeIn(person: Person): void {
+    if (
+      this.door &&
+      this.key.getSignature() === person.getKey().getSignature()
+    ) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(key: Key): void;
+}
+
+
+class MyHouse extends House {
+    openDoor(key:Key):void{
+        if(key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log('The door is open!')
+        }
+        else {
+            console.log('The door remains closed')
+        }
+    }
+}
+
+
 const key = new Key();
 
-const house = new MyHouse(key);
+const house = new MyHouse(false, key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
-
-export {};
